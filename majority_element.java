db04b08d1b4b059e85d99c2adb8acc2cc4877cff
Giuliano <giulianoverdone@gmail.com>
2024-05-17 20:35:22 -0400

    Create majority_element.java

---
+++ majority_element.java
@@ -0,0 +1,27 @@
+// LeetCode Problem #169: Majority Element
+// Category: Array/String
+
+class Solution {
+    public int majorityElement(int[] nums) {
+
+        int majorityElement = 0;
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (count == 0) {
+                majorityElement = nums[i];
+            }
+
+            if (nums[i] == majorityElement) {
+                count++;
+            } else {
+                count--;
+            }
+
+        }
+
+        return majorityElement;
+
+    }
+}
