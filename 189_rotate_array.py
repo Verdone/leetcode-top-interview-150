54e7a9ce0696552ee6fa64c7f65a92c655240977
Giuliano <giulianoverdone@gmail.com>
2024-09-10 21:58:44 -0400

    Create 189_rotate_array.py

---
+++ 189_rotate_array.py
@@ -0,0 +1,25 @@
+# 226. Invert Binary Tree
+# Category: Binary Tree General
+# Difficulty: Easy
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        k=k%len(nums)
+        l,r=0,len(nums)-1
+        while l<r:
+            nums[l],nums[r]=nums[r],nums[l]
+            l+=1
+            r-=1
+        l,r=0,k-1
+        while l<r:
+            nums[l],nums[r]=nums[r],nums[l]
+            l+=1
+            r-=1
+        l,r=k,len(nums)-1
+        while l<r:
+            nums[l],nums[r]=nums[r],nums[l]
+            l+=1
+            r-=1
